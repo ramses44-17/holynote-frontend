1804c3c950717269565a4336e761874aa7cd8809
amos <amoskalunga44@gmail.com>
2025-06-20 16:39:16 +0100

    fix not found page lors de l'actualisation

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import mkcert from'vite-plugin-mkcert'
+import mkcert from 'vite-plugin-mkcert'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),mkcert()],
+  plugins: [react(), tailwindcss(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }
+  },
+  build: {
+    outDir: 'dist', // <-- important pour Vercel
+  },
+  base: '/', // <-- crucial pour que Vercel serve correctement les routes
 })
